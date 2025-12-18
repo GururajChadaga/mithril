2c58a0fef33eb5cd3e03c2e4562035dbc4ae6c89
Gururaj Chadaga <guruuraj.chadaga@gmail.com>
2025-12-18 21:51:08 +0530

    added model User

---
+++ simple-app/src/models/User.js
@@ -0,0 +1,18 @@
+import m from 'mithril';
+const usersUrl = 'https://jsonplaceholder.typicode.com/users';
+
+const User = {
+  list: [],
+  loadList: function () {
+    return m
+      .request({
+        method: 'GET',
+        url: usersUrl,
+      })
+      .then(function (result) {
+        User.list = result.data;
+      });
+  },
+};
+
+export default User;
